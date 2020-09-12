985f7b0c5e3a6405262cbc801f0c90ffed5215b3
Chris <bethel.chris@gmail.com>
2020-09-12 12:03:20 -0400

    Render index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,15 +7,6 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        // require authentication
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         return Inertia::render('Dashboard');
